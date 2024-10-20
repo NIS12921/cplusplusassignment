fea464506934b64bb06194073cc6ed76bde3ff79
NIS12921 <nurislam150803@gmail.com>
2024-10-21 03:08:31 +0530

    Update tutorial1.1.cpp

--- assignment1/tutorial1.1.cpp
+++ assignment1/tutorial1.1.cpp
@@ -1,1 +1,15 @@
 
+#include<iostream>
+int main()
+{
+  int x,y,d,w;
+  std::cout<<"ENTER DAYS ARE : ";
+  std::cin>>x;
+  y=x/365;
+  x=x%365;
+  w=x/7;
+  d=d%7;
+  std::cout<<" years: "<<y<< " weeks: "<< w <<" days: "<<d<< "\n";
+  return 0; 
+}
+
